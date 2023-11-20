3aec70099dcdf8255aede5e945c391181a4d6860
Fernando Fuertes <fer@desprogramar.com>
2023-11-20 11:15:13 +0100

    Custom Path Aliases with TypeScript
    https://reactnative.dev/docs/typescript#using-custom-path-aliases-with-typescript
    
    yarn add -D babel-plugin-module-resolver

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,24 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.json',
+          '.native.js',
+        ],
+        alias: {
+          '^#(.+)': './src/\\1',
+        },
+      },
+    ],
+  ],
 };
